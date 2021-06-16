c35e3b1c32b87a3c811b8a810bbfcbd2221e8889
sarosh <sarosh.tariq@kalsym.com>
2021-06-16 11:06:50 +0500

    added email template

--- src/main/java/com/kalsym/email/service/controller/EmailController.java
+++ src/main/java/com/kalsym/email/service/controller/EmailController.java
@@ -1,23 +1,25 @@
 package com.kalsym.email.service.controller;
 
 import com.kalsym.email.service.model.HttpReponse;
-import com.kalsym.email.service.model.EmailBody;
+import com.kalsym.email.service.model.Email;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.kalsym.email.service.EmailServiceApplication;
 import com.kalsym.email.service.util.Logger;
+import java.util.Arrays;
+import java.util.Date;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.kalsym.email.service.util.EmailUtil;
 
 /**
  *
@@ -30,37 +32,57 @@ public class EmailController {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     @Value("${session.expiry:3600}")
     private int expiry;
 
     @Value("${symplified.noreply.email.from:no-reply@symplified.biz}")
     private String noReplyFrom;
 
+    @Value("${symplified.email.template.path:D:/kalsym-repo/Symplified/email-service/src/main/resources/templates/email/}")
+    private String emailTemplatePath;
+
+    @Value("${symplified.logo.path:https://www.kalsym.com/sym-logo.png}")
+    private String symplifiedLogoPath;
+
     @PostMapping(path = {"/no-reply"}, name = "post-email-noreply")
-     @PreAuthorize("hasAnyAuthority('post-email-noreply', 'all')")
+    @PreAuthorize("hasAnyAuthority('post-email-noreply', 'all')")
     public ResponseEntity<HttpReponse> postNoReplyEmail(HttpServletRequest request,
-            @RequestBody EmailBody body) {
+            @RequestBody Email body) {
         String logprefix = request.getRequestURI();
         HttpReponse response = new HttpReponse(request.getRequestURI());
-        Logger.application.info(Logger.pattern, EmailServiceApplication.VERSION, logprefix, "", "");
+        Logger.application.info(Logger.pattern, EmailServiceApplication.VERSION, logprefix, "body: " + body, "");
 
         try {
-            SimpleMailMessage msg = new SimpleMailMessage();
-            //msg.setTo("to_1@gmail.com", "to_2@gmail.com", "to_3@yahoo.com");
-            msg.setFrom(noReplyFrom);
-            msg.setTo(body.getTo());
 
-            msg.setSubject(body.getSubject());
-            msg.setText(body.getBody());
+            MimeMessage message = mailSender.createMimeMessage();
 
-            javaMailSender.send(msg);
-        } catch (MailException e) {
+            message.setSubject(body.getBody().getOrderStatus().label + " " + (new Date()).toString());
+            MimeMessageHelper helper;
+            helper = new MimeMessageHelper(message, true);
+
+            String emailBody = EmailUtil.generateEmail(body.getBody(), emailTemplatePath, symplifiedLogoPath);
+
+            helper.setFrom(noReplyFrom);
+            helper.setTo(body.getTo());
+            helper.setText(emailBody, true);
+            Logger.application.info(Logger.pattern, EmailServiceApplication.VERSION, logprefix, "email subject: " + body.getSubject(), "");
+
+            mailSender.send(message);
+            Logger.application.info(Logger.pattern, EmailServiceApplication.VERSION, logprefix, "email sent from: " + noReplyFrom, "");
+
+        } catch (Exception e) {
             Logger.application.error(Logger.pattern, EmailServiceApplication.VERSION, logprefix, "email could not be send", "", e);
             response.setErrorStatus(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
 
+        Logger.application.info(Logger.pattern, EmailServiceApplication.VERSION, logprefix, "email sent to: " + Arrays.toString(body.getTo()), "");
+
         response.setSuccessStatus(HttpStatus.OK);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
 }
---
+++ src/main/java/com/kalsym/email/service/model/EmailBodyContent.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kalsym.email.service.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import com.kalsym.email.service.model.order.*;
+import java.util.List;
+
+/**
+ *
+ * @author saros
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ToString
+public class EmailBodyContent {
+
+    public enum OrderStatus {
+        DELIVERED_TO_CUSTOMER("Order Delivered"),
+        PAYMENT_CONFIRMED("Payment Confirmed"),
+        BEING_DELIVERED("Order on its way!"),
+        CANCELED_BY_CUSTOMER("Order cancelation confirmed"),
+        READY_FOR_DELIVERY("Order is processed and awating delivery"),
+        RECEIVED_AT_STORE("Order received at store"),
+        REFUNDED("Refund confirmation");
+
+        public final String label;
+
+        private OrderStatus(String label) {
+            this.label = label;
+        }
+    }
+    private String title;
+    private String storeName;
+    private String storeAddress;
+
+    private OrderStatus orderStatus;
+
+    private String receiverName;
+    private String deliveryAddress;
+    private String deliveryCity;
+
+    private List<OrderItem> orderItems;
+
+    private String invoiceId;
+    private String deliveryCharges;
+    private String subTotal;
+}
