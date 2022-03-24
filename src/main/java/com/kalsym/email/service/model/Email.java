aedc3a18d03eb2539b6b4eabfea5bb8f80b19ad4
taufik <taufik@kalsym.com>
2022-03-24 14:52:36 +0800

    new version 3.0.10

--- src/main/java/com/kalsym/email/service/model/Email.java
+++ src/main/java/com/kalsym/email/service/model/Email.java
@@ -28,5 +28,6 @@ public class Email {
     private String rawBody;
     private OrderEmailBodyContent body;
     private AccountVerificationEmailBody userAccountBody;
+    private String domain;
     
 }
